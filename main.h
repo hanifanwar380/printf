16084a07f0a4ca8dfa995dc5a0939c133e4cb0e4
Mauryn7 <101446517+Mauryn7@users.noreply.github.com>
2022-05-22 21:07:14 +0300

    updated main.h

--- main.h
+++ main.h
@@ -13,9 +13,9 @@
  */
 typedef struct flags
 {
-	int plus;
-	int space;
-	int hash;
+    int plus;
+    int space;
+    int hash;
 } flags_t;
 
 /**
@@ -26,8 +26,8 @@ typedef struct flags
  */
 typedef struct printHandler
 {
-	char c;
-	int (*f)(va_list ap, flags_t *f);
+    char c;
+    int (*f)(va_list ap, flags_t *f);
 } ph;
 
 /* print_nums */
@@ -72,5 +72,6 @@ int print_address(va_list l, flags_t *f);
 
 /* print_percent */
 int print_percent(va_list l, flags_t *f);
+
 
 #endif
